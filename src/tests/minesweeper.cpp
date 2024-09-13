75157c78719092a5268732aedb405b911e6587f1
Nic Mews-Schmuck <resetcentral@proton.me>
2024-09-13 12:04:36 -0500

    toggle_flag tests

--- src/tests/minesweeper.cpp
+++ src/tests/minesweeper.cpp
@@ -41,6 +41,63 @@ TEST(MinesweeperGetTile, OutOfBoundsY) {
     EXPECT_THROW(game.get_tile(3, 12), std::out_of_range);
 }
 
+TEST(MinesweeperToggleFlag, OutOfBoundsX) {
+    Minesweeper game ( 9, 12, 14 );
+    EXPECT_THROW(game.toggle_flag(9, 5), std::out_of_range);
+}
+
+TEST(MinesweeperToggleFlag, OutOfBoundsY) {
+    Minesweeper game ( 9, 12, 14 );
+    EXPECT_THROW(game.toggle_flag(3, 12), std::out_of_range);
+}
+
+TEST(MinesweeperToggleFlag, OnAndOff) {
+    Minesweeper game ( 9, 12, 14 );
+
+    EXPECT_EQ(game.get_tile(0, 0), Minesweeper::COVERED);
+    EXPECT_EQ(game.flags_placed_count(), 0);
+    game.toggle_flag(0, 0);
+    EXPECT_EQ(game.get_tile(0, 0), Minesweeper::FLAG);
+    EXPECT_EQ(game.flags_placed_count(), 1);
+
+    game.toggle_flag(0, 0);
+    EXPECT_EQ(game.get_tile(0, 0), Minesweeper::COVERED);
+    EXPECT_EQ(game.flags_placed_count(), 0);
+}
+
+TEST(MinesweeperToggleFlag, MultiFlag) {
+    Minesweeper game ( 9, 12, 14 );
+
+    EXPECT_EQ(game.get_tile(1, 5), Minesweeper::COVERED);
+    EXPECT_EQ(game.get_tile(3, 7), Minesweeper::COVERED);
+    EXPECT_EQ(game.get_tile(8, 2), Minesweeper::COVERED);
+    EXPECT_EQ(game.flags_placed_count(), 0);
+
+    game.toggle_flag(1, 5);
+    EXPECT_EQ(game.get_tile(1, 5), Minesweeper::FLAG);
+    EXPECT_EQ(game.flags_placed_count(), 1);
+
+    game.toggle_flag(3, 7);
+    EXPECT_EQ(game.get_tile(3, 7), Minesweeper::FLAG);
+    EXPECT_EQ(game.flags_placed_count(), 2);    
+
+    game.toggle_flag(8, 2);
+    EXPECT_EQ(game.get_tile(8, 2), Minesweeper::FLAG);
+    EXPECT_EQ(game.flags_placed_count(), 3);
+
+    game.toggle_flag(3, 7);
+    EXPECT_EQ(game.get_tile(3, 7), Minesweeper::COVERED);
+    EXPECT_EQ(game.flags_placed_count(), 2);  
+
+    game.toggle_flag(1, 5);
+    EXPECT_EQ(game.get_tile(1, 5), Minesweeper::COVERED);
+    EXPECT_EQ(game.flags_placed_count(), 1);
+
+    game.toggle_flag(8, 2);
+    EXPECT_EQ(game.get_tile(8, 2), Minesweeper::COVERED);
+    EXPECT_EQ(game.flags_placed_count(), 0);
+}
+
 // uncover tile tests
 // - 5
 // - 0 in middle

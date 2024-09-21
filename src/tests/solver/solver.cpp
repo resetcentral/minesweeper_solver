5a379b84eb2c635ef1ba03171cbcaaf1402952f7
Nic Mews-Schmuck <resetcentral@proton.me>
2024-09-21 13:37:48 -0500

    probability test

--- src/tests/solver/solver.cpp
+++ src/tests/solver/solver.cpp
@@ -149,67 +149,6 @@ TEST_F(SubSolverTest, AdvancedSolve2Safe) {
 
 }
 
-TEST_F(SubSolverTest, AdvancedSolveSetProbability) {
-    minesweeper::Minefield probable_field = {
-        { Minesweeper::COVERED, 1,                    Minesweeper::COVERED },
-        { Minesweeper::COVERED, 2,                    Minesweeper::COVERED },
-        { Minesweeper::COVERED, Minesweeper::COVERED, Minesweeper::COVERED },
-        { Minesweeper::COVERED, Minesweeper::COVERED, Minesweeper::COVERED }
-    };
-    auto state = SolverState(probable_field);
-
-    EXPECT_EQ(advanced.solve(state, 3).size(), 0);
-
-    boost::rational<unsigned int> one_third {1, 3};
-    boost::rational<unsigned int> one_quarter {1, 4};
-
-    EXPECT_EQ(state.get_node(0, 0)->mine_probability(), one_quarter);
-    EXPECT_EQ(state.get_node(0, 2)->mine_probability(), one_quarter);
-    EXPECT_EQ(state.get_node(1, 0)->mine_probability(), one_quarter);
-    EXPECT_EQ(state.get_node(1, 2)->mine_probability(), one_quarter);
-    EXPECT_EQ(state.get_node(2, 0)->mine_probability(), one_third);
-    EXPECT_EQ(state.get_node(2, 1)->mine_probability(), one_third);
-    EXPECT_EQ(state.get_node(2, 2)->mine_probability(), one_third);
-}
-
-TEST_F(SubSolverTest, AdvancedSolveSetProbabilityMultiple) {
-    minesweeper::Minefield probable_field = {
-        { Minesweeper::COVERED, 1,                    Minesweeper::COVERED },
-        { Minesweeper::COVERED, Minesweeper::COVERED, Minesweeper::COVERED },
-        { Minesweeper::COVERED, 2,                    Minesweeper::COVERED },
-        { Minesweeper::COVERED, 1,                    Minesweeper::COVERED }
-    };
-
-    // a   b
-    // c d e
-    // f   g
-    // h   i
-    // L = 2, O = 3, R = 4
-    // (2c1) * (3c0) * (4c2) = 12
-    // (2c1) * (3c1) * (4c0) = 12
-    // L = 24/24, O = 12/24, R = 12*6/24
-    // l = 12/24, o = 4/24,  r = 3*6/24
-    // l = 1/2,   o = 1/6,   r = 3/4
-    
-    auto state = SolverState(probable_field);
-
-    EXPECT_EQ(advanced.solve(state, 3).size(), 0);
-
-    boost::rational<unsigned int> three_eighths {3, 8};
-    boost::rational<unsigned int> one_third {1, 3};
-    boost::rational<unsigned int> one_quarter {1, 4};
-
-    EXPECT_EQ(state.get_node(0, 0)->mine_probability(), three_eighths);
-    EXPECT_EQ(state.get_node(0, 2)->mine_probability(), three_eighths);
-    EXPECT_EQ(state.get_node(1, 0)->mine_probability(), one_third);
-    EXPECT_EQ(state.get_node(1, 1)->mine_probability(), one_third);
-    EXPECT_EQ(state.get_node(1, 2)->mine_probability(), one_third);
-    EXPECT_EQ(state.get_node(2, 0)->mine_probability(), one_quarter);
-    EXPECT_EQ(state.get_node(2, 2)->mine_probability(), one_quarter);
-    EXPECT_EQ(state.get_node(3, 0)->mine_probability(), one_quarter);
-    EXPECT_EQ(state.get_node(3, 2)->mine_probability(), one_quarter);
-}
-
 TEST_F(SubSolverTest, ProbableSolve1LowMines) {
     minesweeper::Minefield probable_field = {
         { Minesweeper::COVERED, 1,                    Minesweeper::COVERED },
@@ -258,5 +197,21 @@ TEST_F(SubSolverTest, ProbableSolve2) {
     EXPECT_THAT(safeish, AnyOf(
         state.get_node(0, 0),                       state.get_node(0, 2), 
         state.get_node(1, 0), state.get_node(1, 1), state.get_node(1, 2)
+    ));
+}
+
+TEST_F(SubSolverTest, ProbableSolveHard) {
+    minesweeper::Minefield probable_hard_field = {
+        { Minesweeper::COVERED, 2,                    Minesweeper::COVERED },
+        { Minesweeper::COVERED, Minesweeper::COVERED, 1 },
+        { Minesweeper::COVERED, 2,                    Minesweeper::COVERED}
+    };
+    auto state = SolverState(probable_hard_field);
+
+    auto safeish = probable.solve(state, 3);
+
+    EXPECT_THAT(safeish, AnyOf(
+        state.get_node(0, 2),
+        state.get_node(2, 2)
     ));
 }

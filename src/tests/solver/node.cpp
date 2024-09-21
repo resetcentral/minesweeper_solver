1ee7c3f1ca2e8f89f2e3522663db0c23fb93b1e7
Nic Mews-Schmuck <resetcentral@proton.me>
2024-09-21 12:33:36 -0500

    fix node tests to use pointers

--- src/tests/solver/node.cpp
+++ src/tests/solver/node.cpp
@@ -39,32 +39,32 @@ TEST(SolverNode, SetMineProbabilityThrowHigh) {
 
 class NodeTest : public ::testing::Test {
     protected:
-    std::vector<std::vector<Node>> nodes;
+    std::vector<std::vector<Node*>> nodes;
 
     virtual void SetUp() {
+        nodes.resize(3);
         for (auto x = 0; x < 3; x++) {
-            nodes.push_back(std::vector<Node>{});
             for (auto y = 0; y < 3; y++) {
-                nodes[x].push_back(Node(x, y));
+                nodes[x].push_back(new Node(x, y));
             }
         }
 
         for (auto x = 0; x < 3; x++) {
             for (auto y = 0; y < 3; y++) {
                 if (y >= 1) {
-                    nodes[x][y].add_adjacent(&nodes[x][y-1]);
+                    nodes[x][y]->add_adjacent(nodes[x][y-1]);
                 }
                 if (x < 2 && y >= 1) {
-                    nodes[x][y].add_adjacent(&nodes[x+1][y-1]);
+                    nodes[x][y]->add_adjacent(nodes[x+1][y-1]);
                 }
                 if (x < 2) {
-                    nodes[x][y].add_adjacent(&nodes[x+1][y]);
+                    nodes[x][y]->add_adjacent(nodes[x+1][y]);
                 }
                 if (y < 2) {
-                    nodes[x][y].add_adjacent(&nodes[x][y+1]);
+                    nodes[x][y]->add_adjacent(nodes[x][y+1]);
                 }
                 if (x < 2 && y < 2) {
-                    nodes[x][y].add_adjacent(&nodes[x+1][y+1]);
+                    nodes[x][y]->add_adjacent(nodes[x+1][y+1]);
                 }
                 
             }
@@ -77,112 +77,112 @@ class NodeTest : public ::testing::Test {
 };
 
 TEST_F(NodeTest, Setup) {
-    EXPECT_EQ(nodes[0][0].adjacent().size(), 3);
-    EXPECT_EQ(nodes[0][1].adjacent().size(), 5);
-    EXPECT_EQ(nodes[0][2].adjacent().size(), 3);
-    EXPECT_EQ(nodes[1][0].adjacent().size(), 5);
-    EXPECT_EQ(nodes[1][1].adjacent().size(), 8);
-    EXPECT_EQ(nodes[1][2].adjacent().size(), 5);
-    EXPECT_EQ(nodes[2][0].adjacent().size(), 3);
-    EXPECT_EQ(nodes[2][1].adjacent().size(), 5);
-    EXPECT_EQ(nodes[2][2].adjacent().size(), 3);
+    EXPECT_EQ(nodes[0][0]->adjacent().size(), 3);
+    EXPECT_EQ(nodes[0][1]->adjacent().size(), 5);
+    EXPECT_EQ(nodes[0][2]->adjacent().size(), 3);
+    EXPECT_EQ(nodes[1][0]->adjacent().size(), 5);
+    EXPECT_EQ(nodes[1][1]->adjacent().size(), 8);
+    EXPECT_EQ(nodes[1][2]->adjacent().size(), 5);
+    EXPECT_EQ(nodes[2][0]->adjacent().size(), 3);
+    EXPECT_EQ(nodes[2][1]->adjacent().size(), 5);
+    EXPECT_EQ(nodes[2][2]->adjacent().size(), 3);
 }
 
 TEST_F(NodeTest, SetValueCovered) {
-    nodes[1][1].set_value(Minesweeper::COVERED);
-    EXPECT_EQ(nodes[1][1].value(), Minesweeper::COVERED);
+    nodes[1][1]->set_value(Minesweeper::COVERED);
+    EXPECT_EQ(nodes[1][1]->value(), Minesweeper::COVERED);
 }
 
 TEST_F(NodeTest, SetValueFlag) {
-    nodes[1][1].set_value(Minesweeper::FLAG);
-    EXPECT_EQ(nodes[1][1].value(), Minesweeper::FLAG);
-    EXPECT_EQ(nodes[1][1].mine_probability(), 1);
+    nodes[1][1]->set_value(Minesweeper::FLAG);
+    EXPECT_EQ(nodes[1][1]->value(), Minesweeper::FLAG);
+    EXPECT_EQ(nodes[1][1]->mine_probability(), 1);
 }
 
 TEST_F(NodeTest, SetValueNum) {
-    nodes[1][1].set_value(3);
-    EXPECT_EQ(nodes[1][1].value(), 3);
-    EXPECT_EQ(nodes[1][1].mine_probability(), 0);
-    EXPECT_EQ(nodes[1][1].adjacent_mines_left(), 3);
+    nodes[1][1]->set_value(3);
+    EXPECT_EQ(nodes[1][1]->value(), 3);
+    EXPECT_EQ(nodes[1][1]->mine_probability(), 0);
+    EXPECT_EQ(nodes[1][1]->adjacent_mines_left(), 3);
 }
 
 TEST_F(NodeTest, SetValueFlagAdjacentNum) {
-    nodes[0][0].set_value(2);
-    nodes[2][1].set_value(4);
+    nodes[0][0]->set_value(2);
+    nodes[2][1]->set_value(4);
 
-    EXPECT_EQ(nodes[0][0].adjacent_mines_left(), 2);
-    EXPECT_EQ(nodes[2][1].adjacent_mines_left(), 4);
+    EXPECT_EQ(nodes[0][0]->adjacent_mines_left(), 2);
+    EXPECT_EQ(nodes[2][1]->adjacent_mines_left(), 4);
 
-    nodes[1][1].set_value(Minesweeper::FLAG);
+    nodes[1][1]->set_value(Minesweeper::FLAG);
 
-    EXPECT_EQ(nodes[0][0].adjacent_mines_left(), 1);
-    EXPECT_EQ(nodes[2][1].adjacent_mines_left(), 3);
+    EXPECT_EQ(nodes[0][0]->adjacent_mines_left(), 1);
+    EXPECT_EQ(nodes[2][1]->adjacent_mines_left(), 3);
 }
 
 TEST_F(NodeTest, SetValueNumAdjacentFlag) {
-    nodes[0][0].set_value(Minesweeper::FLAG);
-    nodes[2][1].set_value(Minesweeper::FLAG);
-    nodes[1][1].set_value(3);
+    nodes[0][0]->set_value(Minesweeper::FLAG);
+    nodes[2][1]->set_value(Minesweeper::FLAG);
+    nodes[1][1]->set_value(3);
 
-    EXPECT_EQ(nodes[1][1].adjacent_mines_left(), 1);
+    EXPECT_EQ(nodes[1][1]->adjacent_mines_left(), 1);
 }
 
 TEST_F(NodeTest, AdjacentCovered) {
-    nodes[1][0].set_value(3);
-    nodes[0][1].set_value(2);
-    nodes[1][2].set_value(5);
+    nodes[1][0]->set_value(3);
+    nodes[0][1]->set_value(2);
+    nodes[1][2]->set_value(5);
 
-    EXPECT_THAT(nodes[0][0].adjacent_covered(), 
-        UnorderedElementsAre(&nodes[1][1]));
+    EXPECT_THAT(nodes[0][0]->adjacent_covered(), 
+        UnorderedElementsAre(nodes[1][1]));
 
-    EXPECT_THAT(nodes[2][0].adjacent_covered(), 
-        UnorderedElementsAre(&nodes[1][1], &nodes[2][1]));
+    EXPECT_THAT(nodes[2][0]->adjacent_covered(), 
+        UnorderedElementsAre(nodes[1][1], nodes[2][1]));
 
-    EXPECT_THAT(nodes[1][1].adjacent_covered(), 
-        UnorderedElementsAre(&nodes[0][0], &nodes[0][2], &nodes[2][0], &nodes[2][1], &nodes[2][2]));
+    EXPECT_THAT(nodes[1][1]->adjacent_covered(), 
+        UnorderedElementsAre(nodes[0][0], nodes[0][2], nodes[2][0], nodes[2][1], nodes[2][2]));
 }
 
 TEST_F(NodeTest, AdjacentCoveredCount) {
-    nodes[1][0].set_value(3);
-    nodes[0][1].set_value(2);
-    nodes[1][2].set_value(5);
+    nodes[1][0]->set_value(3);
+    nodes[0][1]->set_value(2);
+    nodes[1][2]->set_value(5);
 
-    EXPECT_EQ(nodes[0][0].adjacent_covered_count(), 1);
-    EXPECT_EQ(nodes[2][0].adjacent_covered_count(), 2);
-    EXPECT_EQ(nodes[1][1].adjacent_covered_count(), 5);
+    EXPECT_EQ(nodes[0][0]->adjacent_covered_count(), 1);
+    EXPECT_EQ(nodes[2][0]->adjacent_covered_count(), 2);
+    EXPECT_EQ(nodes[1][1]->adjacent_covered_count(), 5);
 }
 
 TEST_F(NodeTest, CoveredEdge) {
-    nodes[0][0].set_value(1);
-    nodes[0][1].set_value(1);
-    nodes[1][0].set_value(1);
+    nodes[0][0]->set_value(1);
+    nodes[0][1]->set_value(1);
+    nodes[1][0]->set_value(1);
     
-    EXPECT_FALSE(nodes[0][0].covered_edge());
-    EXPECT_FALSE(nodes[0][1].covered_edge());
-    EXPECT_TRUE(nodes[0][2].covered_edge());
-    EXPECT_FALSE(nodes[1][0].covered_edge());
-    EXPECT_TRUE(nodes[1][1].covered_edge());
-    EXPECT_TRUE(nodes[1][2].covered_edge());
-    EXPECT_TRUE(nodes[2][0].covered_edge());
-    EXPECT_TRUE(nodes[2][1].covered_edge());
-    EXPECT_FALSE(nodes[2][2].covered_edge());
+    EXPECT_FALSE(nodes[0][0]->covered_edge());
+    EXPECT_FALSE(nodes[0][1]->covered_edge());
+    EXPECT_TRUE(nodes[0][2]->covered_edge());
+    EXPECT_FALSE(nodes[1][0]->covered_edge());
+    EXPECT_TRUE(nodes[1][1]->covered_edge());
+    EXPECT_TRUE(nodes[1][2]->covered_edge());
+    EXPECT_TRUE(nodes[2][0]->covered_edge());
+    EXPECT_TRUE(nodes[2][1]->covered_edge());
+    EXPECT_FALSE(nodes[2][2]->covered_edge());
 }
 
 TEST_F(NodeTest, NumberEdge) {
-    nodes[0][0].set_value(1);
-    nodes[0][1].set_value(1);
-    nodes[1][0].set_value(1);
-    nodes[1][1].set_value(1);
+    nodes[0][0]->set_value(1);
+    nodes[0][1]->set_value(1);
+    nodes[1][0]->set_value(1);
+    nodes[1][1]->set_value(1);
     
-    EXPECT_FALSE(nodes[0][0].number_edge());
-    EXPECT_TRUE(nodes[0][1].number_edge());
-    EXPECT_FALSE(nodes[0][2].number_edge());
-    EXPECT_TRUE(nodes[1][0].number_edge());
-    EXPECT_TRUE(nodes[1][1].number_edge());
-    EXPECT_FALSE(nodes[1][2].number_edge());
-    EXPECT_FALSE(nodes[2][0].number_edge());
-    EXPECT_FALSE(nodes[2][1].number_edge());
-    EXPECT_FALSE(nodes[2][2].number_edge());
+    EXPECT_FALSE(nodes[0][0]->number_edge());
+    EXPECT_TRUE(nodes[0][1]->number_edge());
+    EXPECT_FALSE(nodes[0][2]->number_edge());
+    EXPECT_TRUE(nodes[1][0]->number_edge());
+    EXPECT_TRUE(nodes[1][1]->number_edge());
+    EXPECT_FALSE(nodes[1][2]->number_edge());
+    EXPECT_FALSE(nodes[2][0]->number_edge());
+    EXPECT_FALSE(nodes[2][1]->number_edge());
+    EXPECT_FALSE(nodes[2][2]->number_edge());
 }
 
 TEST_F(NodeTest, CoveredSafe) {
@@ -191,17 +191,17 @@ TEST_F(NodeTest, CoveredSafe) {
      * O O 1
      * O O O
     */
-    nodes[0][0].set_value(2);
-    nodes[2][1].set_value(1);
-    nodes[1][0].set_value(Minesweeper::FLAG);
+    nodes[0][0]->set_value(2);
+    nodes[2][1]->set_value(1);
+    nodes[1][0]->set_value(Minesweeper::FLAG);
 
-    EXPECT_FALSE(nodes[0][0].covered_safe());
-    EXPECT_FALSE(nodes[0][1].covered_safe());
-    EXPECT_FALSE(nodes[0][2].covered_safe());
-    EXPECT_FALSE(nodes[1][0].covered_safe());
-    EXPECT_TRUE(nodes[1][1].covered_safe());
-    EXPECT_TRUE(nodes[1][2].covered_safe());
-    EXPECT_TRUE(nodes[2][0].covered_safe());
-    EXPECT_FALSE(nodes[2][1].covered_safe());
-    EXPECT_TRUE(nodes[2][2].covered_safe());
+    EXPECT_FALSE(nodes[0][0]->covered_safe());
+    EXPECT_FALSE(nodes[0][1]->covered_safe());
+    EXPECT_FALSE(nodes[0][2]->covered_safe());
+    EXPECT_FALSE(nodes[1][0]->covered_safe());
+    EXPECT_TRUE(nodes[1][1]->covered_safe());
+    EXPECT_TRUE(nodes[1][2]->covered_safe());
+    EXPECT_TRUE(nodes[2][0]->covered_safe());
+    EXPECT_FALSE(nodes[2][1]->covered_safe());
+    EXPECT_TRUE(nodes[2][2]->covered_safe());
 }
